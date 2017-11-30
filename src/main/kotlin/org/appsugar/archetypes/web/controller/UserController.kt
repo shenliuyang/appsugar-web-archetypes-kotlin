97dce482a41e8b737057cb6586ba72f128018f40
NewYoung <shenliuyang@gmail.com>
2017-11-30 13:02:48 +0800

    add page

--- src/main/kotlin/org/appsugar/archetypes/web/controller/UserController.kt
+++ src/main/kotlin/org/appsugar/archetypes/web/controller/UserController.kt
@@ -4,6 +4,7 @@ package org.appsugar.archetypes.web.controller
 import org.appsugar.archetypes.condition.UserCondition
 import org.appsugar.archetypes.repository.UserRepository
 import org.appsugar.archetypes.repository.UserSpecification
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 @Controller
 @RequestMapping("/system/user")
 class UserController(val repository:UserRepository) {
-	@RequestMapping()
-	fun list(condition:UserCondition,model:Model)=model.addAttribute("users",repository.findAll(UserSpecification(condition))).let { "/system/user/list" }
+	@RequestMapping
+	fun list(condition:UserCondition,pageable:Pageable,model:Model)=model.addAttribute("page",repository.findAll(UserSpecification(condition),pageable)).let { "/system/user/list" }
 }
