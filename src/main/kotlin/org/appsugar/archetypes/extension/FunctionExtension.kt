f1191cb41ae724e7f635385effb5cd1328bbc463
谌浏洋 <shenliuyang@ggxueche.com>
2018-06-23 00:22:09 +0800

    add  cloud config support

--- src/main/kotlin/org/appsugar/archetypes/extension/FunctionExtension.kt
+++ src/main/kotlin/org/appsugar/archetypes/extension/FunctionExtension.kt
@@ -2,7 +2,6 @@ package org.appsugar.archetypes.extension
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.ui.Model
 
 
 /****/
@@ -28,9 +27,4 @@ inline fun Long?.notZero(block: Long.() -> Unit): Long? {
     }
     return this
 }
-
-/**
- * for spring mvc
- */
-fun Model.attr(name: String, value: Any) = this.addAttribute(name, value)
 
