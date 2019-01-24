5c65dbd3c82262485cdbc31e2c9a1046b5c99e00
NewYoung <shenliuyang@gmail.com>
2019-01-24 10:42:52 +0800

    upgrade to kotlin 1.3.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,19 +5,19 @@ import org.springframework.boot.gradle.tasks.run.BootRun
 
 buildscript {
     val repos by extra { listOf("http://maven.aliyun.com/nexus/content/groups/public","https://jcenter.bintray.com/") }
-    extra["kotlin.version"] = "1.3.11"
+    extra["kotlin.version"] = "1.3.20"
     repositories { for (u in repos) { maven(u) } }
 }
 
 plugins {
-    val kotlinVersion = "1.3.11"
+    val kotlinVersion = "1.3.20"
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
     kotlin("jvm") version kotlinVersion
     kotlin("kapt") version kotlinVersion
     idea
     id("net.researchgate.release") version "2.7.0"
-    id("org.springframework.boot") version "2.1.1.RELEASE"
+    id("org.springframework.boot") version "2.1.2.RELEASE"
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
 }
 val repos:List<String> by extra
