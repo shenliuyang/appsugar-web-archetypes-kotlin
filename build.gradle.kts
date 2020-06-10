b6efc9d19e388ec72c578202829dacd2aee5e36f
newyoung <shen.ly@vivebest.com>
2020-06-11 00:15:36 +0800

    add env reload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("io.freefair.lombok") version "5.1.0"
     id("net.researchgate.release") version "2.8.0"
     id("org.springframework.boot") version "2.3.0.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
@@ -7,13 +8,21 @@ plugins {
 }
 val repos = listOf("http://maven.aliyun.com/nexus/content/groups/public", "https://jcenter.bintray.com/")
 repositories { repos.forEach(::maven) }
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:Hoxton.SR5")
+    }
+}
 val springBootAdminVersion:String by project
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-config") { exclude(module="spring-cloud-config-client") }
     implementation("org.jolokia:jolokia-core")
     implementation("de.codecentric:spring-boot-admin-starter-client:$springBootAdminVersion")
     implementation("org.springframework.boot:spring-boot-devtools")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok:1.18.12")
     testImplementation("org.apache.ant:ant:1.10.1")
     testImplementation("org.dbunit:dbunit:2.5.4")
     testImplementation("org.springframework.boot:spring-boot-starter-test") { exclude("junit") }
--- src/main/java/org/appsugar/archetypes/Application.java
+++ src/main/java/org/appsugar/archetypes/Application.java
@@ -1,11 +1,33 @@
 package org.appsugar.archetypes;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    private ReloadProperty reloadProperty;
+
+    @GetMapping("/")
+    public String reload() {
+        return reloadProperty.name;
+    }
+
+    @Data
+    @Component
+    @ConfigurationProperties("db")
+    public static class ReloadProperty {
+        private String name;
     }
 }
