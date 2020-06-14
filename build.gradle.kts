dac0c1963a8f858a3ab9731d2262e3c9c61aef52
newyoung <shen.ly@vivebest.com>
2020-06-14 22:26:08 +0800

    add querydsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,12 +23,15 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-config") { exclude(module="spring-cloud-config-client") }
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.querydsl:querydsl-jpa")
     implementation("com.h2database:h2")
     implementation("mysql:mysql-connector-java")
     implementation("de.codecentric:spring-boot-admin-starter-client:$springBootAdminVersion")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok:1.18.12")
+    annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("com.querydsl:querydsl-apt:4.3.1:jpa")
+    annotationProcessor("javax.persistence:javax.persistence-api" )
     testImplementation("org.flywaydb:flyway-core")
     testImplementation("org.apache.ant:ant:1.10.1")
     testImplementation("org.dbunit:dbunit:2.5.4")
@@ -42,8 +45,8 @@ dependencies {
 idea {
     module {
         inheritOutputDirs = false
-        outputDir = file("$buildDir/classes/kotlin/main/")
-        testOutputDir = file("$buildDir/classes/kotlin/test/")
+        outputDir = file("$buildDir/classes/java/main/")
+        testOutputDir = file("$buildDir/classes/java/test/")
     }
 }
 tasks {
@@ -54,4 +57,6 @@ tasks {
         testLogging { showStandardStreams = true }
     }
 }
+
+
 springBoot { buildInfo() }
--- src/main/java/org/appsugar/archetypes/Application.java
+++ src/main/java/org/appsugar/archetypes/Application.java
@@ -1,6 +1,7 @@
 package org.appsugar.archetypes;
 
 import lombok.Data;
+import org.appsugar.archetypes.domain.QUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
