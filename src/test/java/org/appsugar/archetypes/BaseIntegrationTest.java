08d896c77026a634ba134033d367cd9b46d8ac83
newyoung <shen.ly@vivebest.com>
2020-07-25 19:21:40 +0800

    add integration yml

--- src/test/java/org/appsugar/archetypes/BaseIntegrationTest.java
+++ src/test/java/org/appsugar/archetypes/BaseIntegrationTest.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+@ActiveProfiles({"test", "integration"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
 @Import(FeignClientsConfiguration.class)
 public abstract class BaseIntegrationTest {
