23a91b9c1f9af6bace35320200e576dde82615d0
newyoung <shen.ly@vivebest.com>
2020-08-16 13:54:39 +0800

    upgrade to gradle 6.6 spring boot 2.3.3

--- src/main/java/org/appsugar/archetypes/Application.java
+++ src/main/java/org/appsugar/archetypes/Application.java
@@ -1,6 +1,7 @@
 package org.appsugar.archetypes;
 
 import lombok.Data;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
+
+    @SneakyThrows
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 
     @Autowired
     private ReloadProperty reloadProperty;
