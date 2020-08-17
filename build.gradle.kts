615b5688b3198b156b0a7695a1a5c53ce4ab1cce
newyoung <shen.ly@vivebest.com>
2020-08-17 11:10:26 +0800

    remove webflux dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ dependencyManagement { imports { mavenBom("org.springframework.cloud:spring-clou
 val springBootAdminVersion: String by project
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-starter-config") { exclude(module = "spring-cloud-config-client") }
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
