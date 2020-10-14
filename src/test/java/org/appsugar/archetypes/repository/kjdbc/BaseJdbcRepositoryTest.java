334fcde4b246bc1f1e33bd935ecd787aa0f10924
shenliuyang <shenliuyang@gmail.com>
2020-10-14 15:23:08 +0800

    解决集成测试不自动导入数据问题

--- src/test/java/org/appsugar/archetypes/repository/kjdbc/BaseJdbcRepositoryTest.java
+++ src/test/java/org/appsugar/archetypes/repository/kjdbc/BaseJdbcRepositoryTest.java
@@ -1,10 +1,13 @@
 package org.appsugar.archetypes.repository.kjdbc;
 
 import org.appsugar.archetypes.BaseTest;
+import org.appsugar.archetypes.repository.DbImportConfiguration;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
 
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(DbImportConfiguration.class)
 public abstract class BaseJdbcRepositoryTest extends BaseTest {
 }
