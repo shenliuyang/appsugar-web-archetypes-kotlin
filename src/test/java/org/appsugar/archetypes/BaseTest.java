420841da9ffbfbbccdd6d830c47a78eab4a105e3
newyoung <shenliuyang@gmail.com>
2021-03-17 00:22:47 +0800

    set up hibernate batch insert and update

--- src/test/java/org/appsugar/archetypes/BaseTest.java
+++ src/test/java/org/appsugar/archetypes/BaseTest.java
@@ -6,15 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.nio.charset.Charset;
-
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 public abstract class BaseTest {
     protected Logger logger = LoggerFactory.getLogger(getClass());
-
-    public static void main(String[] args) {
-        byte[] c = {-30, -128, -114, 51, 48, 57, 67, 50, 51, 50, 69, 48, 66, 57, 66};
-        System.out.println(new String(c, Charset.forName("utf-8")));
-    }
 }
--- src/test/java/org/appsugar/archetypes/repository/jpa/UserRepositoryTest.java
+++ src/test/java/org/appsugar/archetypes/repository/jpa/UserRepositoryTest.java
@@ -1,7 +1,9 @@
 package org.appsugar.archetypes.repository.jpa;
 
 import com.google.common.collect.Lists;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.appsugar.archetypes.domain.User;
 import org.appsugar.archetypes.domain.UserCondition;
 import org.appsugar.archetypes.repository.UserRepository;
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,6 +22,26 @@ public class UserRepositoryTest extends BaseJpaRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
+    @SneakyThrows
+    @Test
+    public void testBatchInsertAndUpdate() {
+        val users = new ArrayList<User>();
+        for (int i = 1; i <= 10; i++) {
+            val user = new User();
+            user.setName("z" + i);
+            user.setAddress("xxx" + i);
+            user.setAge(i);
+            user.setEmail("xxx" + i);
+            user.setLoginName("login" + i);
+            users.add(user);
+        }
+        userRepository.saveAll(users);
+        userRepository.flush();
+        users.subList(0, 5).forEach(e -> e.setAge(18));
+        users.subList(5, 10).forEach(e -> e.setAddress("xxx18"));
+        userRepository.saveAll(users);
+        userRepository.flush();
+    }
 
     @Test
     public void testFindAll() {
@@ -31,7 +54,7 @@ public class UserRepositoryTest extends BaseJpaRepositoryTest {
         String name = "newyoung";
         List<User> result = userRepository.findByName(name);
         log.debug("testFindByName name {} result is {}", name, result);
-        Assertions.assertTrue(!result.isEmpty());
+        Assertions.assertFalse(result.isEmpty());
     }
 
     @Test
